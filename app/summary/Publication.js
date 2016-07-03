66fb92a9e789b43a3ed9059bb41059aac20b2b71
Asif Ahmed <asifuzzaman.ahmed@gmail.com>
2016-07-02 17:15:58 -0700

    Build Separate Summary View

---
+++ app/summary/Publication.js
@@ -0,0 +1,89 @@
+import React, { Component, PropTypes, LinkingIOS } from 'react';
+import { Animated, StyleSheet, View, Text, Dimensions, WebView, TouchableOpacity, Slider } from 'react-native';
+import Sldr from 'react-native-slider';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Communications from 'react-native-communications';
+
+// Styles & Fonts
+import styles from './styles.js';
+
+// Components 
+
+
+
+
+class Publication extends Component {
+	_onPressHeadline(){
+    Linking.openURL('https://www.facebook.com').catch(err => console.error('An error occured', err));
+	}
+	render() {
+		return (
+	    <View>
+
+        {/* First Row => Pub & Graph Info */}
+		    <View style={styles.firstRow}>
+
+		      {/* Publication */}
+			    <View style={styles.publication}>
+			      <Text style={styles.publicationText}>
+			        <Icon name="md-paper" size={17} color="#ffffff"></Icon>
+			        { '   ' }
+			        {this.props.publication}
+			      </Text>
+		      </View>
+
+					{/* MoodScore Score #  */}
+		      <View>
+		        <Text style={styles.publicationText}>
+		          {this.props.moodScore}
+		        </Text>
+		      </View>
+
+					{/* MoodScore Graph  */}
+		      <View style={styles.moodScore}>
+		        <Sldr
+		          minimumValue = {0}
+		          maximumValue = {40}
+		          minimumTrackTintColor = '#ffffff'
+		          maximumTrackTintColor = '#ffffff'
+		          thumbTintColor = '#ffffff'
+		          disabled = { true }
+		          value={this.props.moodScore} />
+	          
+          </View>
+
+	      </View>
+
+				{/* Second Row => Headline Text */}
+	      <View style={styles.headline}>
+
+	        <TouchableOpacity 
+	          onPress={() => Communications.web('https://github.com/facebook/react-native')} 
+	          style={{flex: 1, flexWrap: 'wrap'}}>
+
+			      <Text style={styles.headlineText}
+			        numberOfLines={2}>
+			        {this.props.headline}
+			        { ' ' }
+			      </Text>
+			      
+		      </TouchableOpacity>
+
+	      </View>
+
+	      <View style={styles.cardFooter}>
+	        <Text style={{flex: 15}}>
+	         {' '}
+	        </Text>
+		      <Text style={styles.link}>
+		        <Icon name="ios-link" size={18} color="#5d5d5d"></Icon>
+		      </Text>
+
+	      </View>
+
+	    </View>
+		)
+	}
+}
+
+export default Publication;
---
+++ app/summary/styles.js
@@ -0,0 +1,108 @@
+import { Animated, StyleSheet, View, Text, Dimensions } from 'react-native';
+
+let styles = StyleSheet.create({
+  body: {
+    backgroundColor: '#eaedf1',
+    flex: 1,
+  },
+  firstRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: 25,
+    paddingRight: 25,
+    marginLeft: 15,
+    marginRight: 15,
+    backgroundColor: '#00afd1',
+    shadowColor: '#000000',
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    shadowOffset: {
+      height: 1,
+      width: 0
+    }
+  },
+  publication: {
+    flex: 3
+  },
+  publicationText: {
+    fontFamily: 'Silom',
+    fontSize: 15,
+    // fontWeight: 'bold',
+    color: '#ffffff'
+  },
+  headline: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    paddingLeft: 25,
+    paddingRight: 25,
+    paddingTop: 15,
+    paddingBottom: 5,
+    marginLeft: 15,
+    marginRight: 15,
+    borderBottomColor: '#eaedf1',
+    backgroundColor: '#ffffff',
+    shadowColor: '#000000',
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    shadowOffset: {
+      height: 6,
+      width: 0
+    }
+  },
+  headlineText: {
+    flex: 1,
+    flexWrap: 'wrap',
+    fontFamily: 'Oswald',
+    color: '#5d5d5d',
+    fontSize: 20
+  },
+  cardFooter: {
+    flex: 1,
+    flexDirection: 'row',
+    paddingLeft: 25,
+    paddingRight: 25,
+    paddingTop: 5,
+    paddingBottom: 10,
+    marginLeft: 15,
+    marginRight: 15,
+    marginBottom: 11,
+    borderBottomColor: '#eaedf1',
+    backgroundColor: '#ffffff',
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+    shadowColor: '#000000',
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    shadowOffset: {
+      height: 7,
+      width: 0
+    }
+  },
+  link: {
+    flex: 1,
+    alignItems: 'flex-end'
+  },
+  moodScore: {
+    flex: 1,
+    marginLeft: 10
+  },
+  bar: {
+    flex: 1,
+    borderRadius: 5,
+    height: 4,
+    backgroundColor: '#ffffff',
+  },
+  trendRow: {
+    flex: 1,
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 10
+  },
+  full: {
+    flex: 1
+  }
+});
+
+export default styles;
